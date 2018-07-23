c2a1d2c26c5f3281f661b6eb048a27912b8d1480
redvg <redvg17@gmail.com>
2018-07-23 11:25:55 +0200

    creates app

---
+++ main.py
@@ -0,0 +1,19 @@
+import logging
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    
+    return 'hello from appengine'
+
+
+@app.errorhandler(500)
+def server_error(e):
+
+    logging.exception('An error occurred during a request.')
+    
+    return 'An internal error occurred.', 500
